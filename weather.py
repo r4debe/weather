2cf4bd450b033e6f016e54bccb725663f9533c95
Andy Sundeland <asunde12@gmail.com>
2019-10-14 21:57:57 +0100

    locations api working

--- weather.py
+++ weather.py
@@ -3,9 +3,6 @@
 import requests
 import json
 
-#ibTWRzyxjP94K3Wn66NlQsr3h2qppp3cs 
-
-r = requests.get("http://http://dataservice.accuweather.com/locations/v1/regions/us?offset=2&apikey={bTWRzyxjP94K3Wn66NlQsr3h2qppp3cs}")
-
-#print(r.status_code)
+r = requests.get("https://dataservice.accuweather.com/locations/v1/regions?apikey=bTWRzyxjP94K3Wn66NlQsr3h2qppp3cs")
+print(r.status_code)
 print(r.json())
