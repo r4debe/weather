14b229c1da0135a4873bedd382c7b841fba58678
Andy <andy@laptop.local>
2023-01-26 23:48:11 +0000

    getting current conditions

--- weather.py
+++ weather.py
@@ -25,7 +25,7 @@ def get_locations(url):
 
 
 def make_request(url):
-    print("make_request()")
+#    print("making_request()")
     resp = requests.get(url)
 
     if resp.status_code != 200:
@@ -38,11 +38,11 @@ def make_request(url):
 class Location:
     Key: str
 
-data = {"Version":1,"Key":"712327","Type":"City","Rank":35,"LocalizedName":"Leeds","EnglishName":"Leeds","PrimaryPostalCode":"LS1 6","Region":{"ID":"EUR","LocalizedName":"Europe","EnglishName":"Europe"},"Country":{"ID":"GB","LocalizedName":"United Kingdom","EnglishName":"United Kingdom"},"AdministrativeArea":{"ID":"LDS","LocalizedName":"Leeds","EnglishName":"Leeds","Level":1,"LocalizedType":"Metropolitan Borough","EnglishType":"Metropolitan Borough","CountryID":"GB"},"TimeZone":{"Code":"GMT","Name":"Europe/London","GmtOffset":0.0,"IsDaylightSaving":False,"NextOffsetChange":"2023-03-26T01:00:00Z"},"GeoPosition":{"Latitude":53.798,"Longitude":-1.542,"Elevation":{"Metric":{"Value":67.0,"Unit":"m","UnitType":5},"Imperial":{"Value":219.0,"Unit":"ft","UnitType":0}}},"IsAlias":False,"SupplementalAdminAreas":[{"Level":0,"LocalizedName":"England","EnglishName":"England"}],"DataSets":["AirQualityCurrentConditions","AirQualityForecasts","Alerts","DailyPollenForecast","ForecastConfidence","FutureRadar","MinuteCast","Radar"]} 
+#data = {"Version":1,"Key":"712327","Type":"City","Rank":35,"LocalizedName":"Leeds","EnglishName":"Leeds","PrimaryPostalCode":"LS1 6","Region":{"ID":"EUR","LocalizedName":"Europe","EnglishName":"Europe"},"Country":{"ID":"GB","LocalizedName":"United Kingdom","EnglishName":"United Kingdom"},"AdministrativeArea":{"ID":"LDS","LocalizedName":"Leeds","EnglishName":"Leeds","Level":1,"LocalizedType":"Metropolitan Borough","EnglishType":"Metropolitan Borough","CountryID":"GB"},"TimeZone":{"Code":"GMT","Name":"Europe/London","GmtOffset":0.0,"IsDaylightSaving":False,"NextOffsetChange":"2023-03-26T01:00:00Z"},"GeoPosition":{"Latitude":53.798,"Longitude":-1.542,"Elevation":{"Metric":{"Value":67.0,"Unit":"m","UnitType":5},"Imperial":{"Value":219.0,"Unit":"ft","UnitType":0}}},"IsAlias":False,"SupplementalAdminAreas":[{"Level":0,"LocalizedName":"England","EnglishName":"England"}],"DataSets":["AirQualityCurrentConditions","AirQualityForecasts","Alerts","DailyPollenForecast","ForecastConfidence","FutureRadar","MinuteCast","Radar"]} 
 
-location: Location = dacite.from_dict(Location,data)
-location_key = location.Key
-print(location_key)
+#location: Location = dacite.from_dict(Location,locations)
+#location_key = location.Key
+#print(location_key)
 
 @dataclass
 class Temp:
@@ -62,21 +62,22 @@ class Weather:
     WeatherText: str
     WeatherIcon: int
 #    PrecipitationType: NoneType
+    HasPrecipitation: bool
     IsDayTime: bool
     Temperature: Temperature
     MobileLink: str
     Link: str
 
-data = {'LocalObservationDateTime': '2023-01-24T20:03:00+00:00', 'EpochTime': 1674590580, 'WeatherText': 'Mostly cloudy', 'WeatherIcon': 38, 'HasPrecipitation': False, 'PrecipitationType': None, 'IsDayTime': False, 'Temperature': {'Metric': {'Value': 6.7, 'Unit': 'C', 'UnitType': 17}, 'Imperial': {'Value': 44.0, 'Unit': 'F', 'UnitType': 18}}, 'MobileLink': 'http://www.accuweather.com/en/gb/leeds/ls1-6/current-weather/712327?lang=en-us', 'Link': 'http://www.accuweather.com/en/gb/leeds/ls1-6/current-weather/712327?lang=en-us'}
+#data = {'LocalObservationDateTime': '2023-01-24T20:03:00+00:00', 'EpochTime': 1674590580, 'WeatherText': 'Mostly cloudy', 'WeatherIcon': 38, 'HasPrecipitation': False, 'PrecipitationType': None, 'IsDayTime': False, 'Temperature': {'Metric': {'Value': 6.7, 'Unit': 'C', 'UnitType': 17}, 'Imperial': {'Value': 44.0, 'Unit': 'F', 'UnitType': 18}}, 'MobileLink': 'http://www.accuweather.com/en/gb/leeds/ls1-6/current-weather/712327?lang=en-us', 'Link': 'http://www.accuweather.com/en/gb/leeds/ls1-6/current-weather/712327?lang=en-us'}
 
-conditions: Weather = dacite.from_dict(Weather,data)
-print(conditions.WeatherText)
+#conditions: Weather = dacite.from_dict(Weather,data)
+#print(conditions.WeatherText)
 
 
 def run():
     api_endpoint = 'https://dataservice.accuweather.com'
 
-    apikey = 'jeWkYxYA6jThydPhSUUv0mXUEcFkngqz'
+    apikey = 'kWVZrARj3pEVRX44COsxZkNOuVeqa3Ah'
 
     # If user uses lower case,  we could add all the locations to the dict in lower-case, 
     # then convert
@@ -88,36 +89,35 @@ def run():
     
     search = 'Leeds LDS'
 
-    locations = get_locations('{}/{}?apikey={}&q={}'.format(
-        api_endpoint, city_path,  apikey, search ))
+    url = '{}/{}?apikey={}&q={}'.format(
+        api_endpoint, city_path,  apikey, search )
     
-    location = (countries['Italy'])
-    print ("Country = " + country_code)
+    locations = make_request(url)
 
-#    # Admin area
-#    admin_areas_path = 'locations/v1/adminareas'
-#
-#    admin_areas = get_locations('{}/{}?apikey={}'.format(
-#        api_endpoint, admin_areas_path, apikey))
-#    
-#    admin_area_code = (admin_areas['Leeds'])
-#    print (admin_area_code)
-
-    # Current conditions url http://dataservice.accuweather.com/currentconditions/v1/712327?apikey=Y1HInAn84tkJVg1goICCfpgb2396Kq5t
-
+    location: Location = dacite.from_dict(Location,locations)
+    location_key = location.Key
+    
+     
+    # Conditions
     conditions_path = 'currentconditions/v1'
-    location_key = '712327'
 
     url = '{}/{}/{}?apikey={}'.format(
         api_endpoint, conditions_path, location_key, apikey)
 
     out = make_request(url)
+    conditions: Weather = dacite.from_dict(Weather,out)
+    print("The Current Conditions are:")
+    print("Local Date / Time: " + conditions.LocalObservationDateTime)
+    print("Summary: " + conditions.WeatherText)
+    value = conditions.Temperature.Metric.Value
+    unit = conditions.Temperature.Metric.Unit
+    temp = str(value) + unit
+    print("Temperature: " + temp)
+    rain =  conditions.HasPrecipitation
+    print("Precipitation: " + str(rain))
     
-    get_weather(out)
-    sys.exit(0)
-
-    current_weather = (weather['WeatherText'])
-    print (current_weather)
+#    get_weather(out)
+#    sys.exit(0)
 
     return
 
