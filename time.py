4a50533e90f4bfb84c15508fb31a949d3c21217a
Andy Sunderland <andy@Mickledyke.local>
2024-01-18 15:17:00 +0000

    time.py added

---
+++ time.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+time_long = '13:07:00+00:00'
+#    time_long = (date_time_list[1])
+time_list = time_long.split(":")
+time_1 = (time_list[0])
+time_2 = (time_list[1])
+print("Time: " + str(time_1) + ":" + str(time_2) + "hrs") 
