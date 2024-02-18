8cca7c2681acff75477f480f0e215cc091723c0b
Andy <asunde12@gmail.com>
2024-02-18 11:12:49 +0000

    Using zip to join data and temp data

--- hist_weather.py
+++ hist_weather.py
@@ -9,7 +9,6 @@ import sys
 import os
 
 def get_geo_loc(url):
-    print("URL:", url)
     headers = CaseInsensitiveDict()
     headers["Accept"] = "application/json"
     resp = requests.get(url, headers=headers)
@@ -29,34 +28,33 @@ def get_geo_loc(url):
     location_data['longitude'] = longitude
     location_data['latitude'] = latitude
 
-    print("Location Data:", location_data)
-
     return location_data
 
-
 def get_weather(url):
-    print("URL:", url)
     resp = requests.get(url)
 
     if resp.status_code != 200:
         print(resp.status_code)
         return None
 
-    weather_data = {}
+    time_list = {}
+    temp_list = {}
 
     data = json.loads(resp.text)
 
-    print("weather_data:", data)
+# Extract the dates
+    date_time = data['hourly']['time']
+# Extract temps
+    temps = data['hourly']['temperature_2m']
+# Store in dict
+    time_list['time'] = date_time
+    temp_list['temps'] = temps
 
-    return weather_data
-
+    return time_list, temp_list
 
 def run():
-    
     geo_api_key = '681d3c165dff476aaf8955910bf00ecb'
-
     geo_api_endpoint = 'https://api.geoapify.com'
-
     house_num = '10'
     street_1 = 'Henrietta'
     street_2 = 'Street'
@@ -66,29 +64,23 @@ def run():
 #    country_1 = 'United'
 #    country_2 = 'Kingdom'
 
-    
     coordinates = get_geo_loc('{}/v1/geocode/search?text={}%20{}%20{}%2C%20{}%20{}H%20{}&apiKey={}'.format(
         geo_api_endpoint, house_num, street_1, street_2, town, postcode_1, postcode_2, geo_api_key))
 
-    print("Coordinates:", coordinates)
+    print(coordinates) 
 
-    # weather data
     weather_api_endpoint = 'https://archive-api.open-meteo.com'
-
     latitude = coordinates['latitude']
     longitude = coordinates['longitude']
-
-    start_date = '2023-12-30'
+    start_date = '2023-12-31'
     end_date = '2024-01-01'
     weather_metric = 'temperature_2m'
 
-    weather = get_weather('{}/v1/archive?latitude={}&longitude={}&start_date={}&end_date={}&hourly={}'.format(
+    time_list, temp_list = get_weather('{}/v1/archive?latitude={}&longitude={}&start_date={}&end_date={}&hourly={}'.format(
         weather_api_endpoint, latitude, longitude, start_date, end_date, weather_metric))
 
-    print(weather) 
+    for time, temp in zip(time_list['time'], temp_list['temps']):
+        print("Time:", time, "| Temperature:", temp)
 
 if __name__ == '__main__':
     run()
-
-    
-# https://archive-api.open-meteo.com/v1/archive?latitude=53.7965&longitude=-1.5478&start_date=2024-01-23&end_date=2024-02-06&hourly=temperature_2m
